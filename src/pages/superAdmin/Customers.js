457d67aa61fc5bc8da2204a327a58a3402e7a89a
debolacodes <debolacodes@gmail.com>
2022-04-19 13:16:28 +0100

    feat: Superadmin Customers Page

--- src/pages/superAdmin/Customers.js
+++ src/pages/superAdmin/Customers.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {mainFunctions} from "../../providers/MainProvider";
+
 import Sidebar from '../../components/Sidebar';
 import TopBar from '../../components/TopBar';
 import SummaryBox from '../../components/SummaryBox';
 import Title from '../../components/Title';
-import Tables from '../../components/Tables';
+import AllCustomersTable from './tables/AllCustomersTable';
+
 
 export default function Customers() {
+  const {
+    totalNoOfCustomers,
+    activeNoOfCustomers,
+    activeCustomersToday,
+
+  } = useContext(mainFunctions)
   return (
     <div className='body'>
         <Sidebar />
@@ -18,12 +27,19 @@ export default function Customers() {
             </div>
               <Title title="Overview"/>
               <div className="wrapper">
-                <SummaryBox title="Total No of Customers" value="2,403"/>
-                <SummaryBox title="Active Customers/Day" value="2,400"/>
-                <SummaryBox title="Active Customers Today" value="2,403"/>
+                <SummaryBox 
+                title={totalNoOfCustomers.title} 
+                value={totalNoOfCustomers.value}/>
+                <SummaryBox 
+                title={activeNoOfCustomers.title} 
+                value={activeNoOfCustomers.value}/>
+                <SummaryBox 
+                title={activeCustomersToday.title} 
+                value={activeCustomersToday.value}
+                />
               </div>
 
-              <Tables />
+              <AllCustomersTable />
             </div>
         </div>
     </div>
---
+++ src/pages/superAdmin/tables/AllCustomersTable.js
@@ -0,0 +1,148 @@
+import React, {useState, useContext, useEffect} from 'react'
+import {mainFunctions} from "../../../providers/MainProvider";
+import Tables from '../../../components/Tables';
+import { formatToCurrency, getDateTimeFormatUK } from "../../../utils";
+
+export default function CustomersDataTable() {
+    const {
+        customersData
+      } = useContext(mainFunctions)
+
+  const [searchKey, setSearchKey] = useState("");
+
+  const tableColumns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      sort: true,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      sort: true,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      sort: true, 
+    },
+    {
+      title: "Total Purchase",
+      dataIndex: "totalPurchase",
+      sort: true,
+    },
+    {
+      title: "Purchase Value",
+      dataIndex: "purchaseValue",
+      sort: true,
+    },
+    {
+      title: "Rewards Value",
+      dataIndex: "rewardsValue",
+      sort: true,
+    },
+    {
+      title: "Last Visit",
+      dataIndex: "lastVisit",
+      sort: true,
+    }
+  ];
+ 
+  const handleSearch = (query) => {
+		setSearchKey(query);
+	};
+  const handleStatusFilter = () => {
+
+  }
+  const handleDateFilter = () => {
+    
+  }
+  
+const [filteredTableData, setFilteredTableData] = useState(customersData);
+  
+const dataSource =
+    filteredTableData &&
+      filteredTableData.length > 0
+        ? filteredTableData.map((row) => {
+					return {
+						id: (
+                <div>
+                    ID: {row.id}
+                </div>
+                ),
+            name: (
+							<div>
+								{row.name}
+							</div>
+						),
+						email: (
+							<div>
+								{row.email}
+							</div>
+						),
+						totalPurchase: (
+							<div>
+								{row.totalPurchase}
+							</div>
+						),
+            purchaseValue: (
+							<div>
+								₦{formatToCurrency(row.purchaseValue, 1)}
+							</div>
+						),
+            rewardsValue: (
+							<div>
+								₦{formatToCurrency(row.rewardsValue, 1)}
+							</div>
+						),
+						lastVisit: (
+							<div>
+								{getDateTimeFormatUK(row.lastVisit)}
+							</div>
+						),
+						
+					};
+			  })
+			: [];
+
+useEffect(() => {
+    console.log("adeb")
+    if(searchKey){
+        console.log("adeb")
+        var fd = customersData.filter((thisStore, index) => {
+            var found = true;
+            for(var i = 0; i < tableColumns.length; i++){
+                if(typeof tableColumns[i].search === "undefined" || tableColumns[i].search === true){
+                  console.log(thisStore[tableColumns[i].dataIndex])
+                    if(thisStore[tableColumns[i].dataIndex].toString().toLowerCase().includes(searchKey)){
+                        found = true
+                        break
+                    }else{
+                        // eslint-disable-next-line no-unused-vars
+                        found = false
+                        continue
+                    }
+                }
+            
+            }
+            console.log(found)
+            return found;
+        })
+        setFilteredTableData(fd)
+    }
+  },[searchKey])
+
+
+
+
+return (
+<div>
+    <Tables
+    title="All Customers"
+    dataSource={dataSource}
+    columns={tableColumns}
+    handleSearch={setSearchKey}
+    ></Tables>
+</div>
+)
+}
